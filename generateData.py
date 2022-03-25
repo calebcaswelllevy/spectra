ab89c15e5eb49af3d8f4d30579e7e587f4bc43e5
Caleb Caswell-Levy <calebcaswelllevy@gmail.com>
2022-03-25 10:16:53 +0000

    added a function to generate random data

---
+++ generateData.py
@@ -0,0 +1,45 @@
+import numpy as np, matplotlib.pyplot as plt, pandas as pd
+
+
+def generate( n:int=1000, nPeaks:int = 10, noise:float = 1, peakwidthsVariability:float = 1) -> None:
+    """
+    
+    """
+    peakCenters = np.random.randint(low=20, high=n-20,size=nPeaks)
+    minimumPeakWidth = 5-peakwidthsVariability
+    maximumPeakWidth = 5+peakwidthsVariability
+    peakwidths = np.random.randint(low=minimumPeakWidth, high=maximumPeakWidth, size=nPeaks)
+
+    #an initial string of random numbers centered on 0:
+    data = np.random.normal(0,noise,n)
+
+    #Make the peaks and add them to the data:
+    for center, width in zip(peakCenters, peakwidths):
+        peak = makePeak(width=width)
+        for index1, index2 in enumerate(range(center-width, center+width+1)):
+            data[index2] += peak[index1]
+            
+    data = pd.DataFrame(zip([i for i in range(len(data))], data))
+    return [data, peakCenters]
+
+def makePeak(width:int, height:int=100):
+    """
+    
+    """
+   
+    rightSide = [-x**2 for x in range(width+1)]
+    leftSide = [x for x in rightSide[:0:-1]]
+
+    peak = leftSide+rightSide
+    scale = -height/leftSide[0]
+    peak = [(x*scale)+height for x in peak]
+    
+    return peak
+
+
+
+if __name__ == '__main__':
+    data = generate(noise=10)
+    plt.plot(data[0])
+    #[plt.axvline(x=peak, c="red") for peak in data[1]]
+    plt.show()
