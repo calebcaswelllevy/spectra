60e12edac7ac7cf28a735de6d24e274adccde7e1
Caleb Caswell-Levy <calebcaswelllevy@gmail.com>
2022-03-27 12:14:31 +0100

    Added random height variation to generateData. Started hill climbing algorithm implementation in Palishkar2009Peak

--- Palshikar2009Peak.py
+++ Palshikar2009Peak.py
@@ -1,33 +1,58 @@
+from operator import ne
 from timeit import repeat
 from matplotlib.cbook import index_of
 import numpy as np, pandas as pd, math, matplotlib.pyplot as plt
 from sklearn.covariance import log_likelihood
+from sqlalchemy import true
 import parseData
-from tables import Float32Atom, test
+
 from sklearn.neighbors import KernelDensity
 from sklearn.model_selection import GridSearchCV
+from sklearn.cluster import OPTICS
+from generateData import generate
+from scipy.stats.mstats import mquantiles
 
 
 
 
-class spectrum:
-    def __init__(self, pathToData) -> None:
-        self.data = parseData.parse(self.pathToData)
-        self.kde = self.estimateKernel(self)
+
+class Spectrum:
+    """
+    Spectrum object holds data for a spectrum. It implements the peak finding algorithm
+    """
+    def __init__(self, pathToData = None, data=None, sensitivity = 0.6, width=10) -> None:
+        self.data = parseData.parse(pathToData) if pathToData else data
+        self.kde = self.estimateKernel()
         self.peaks = []
-        self.probabilities = self.evaluateKDE()
+        self.likelihoods = self.kde.score_samples(np.array(self.data[1]).reshape(-1, 1))
+        
+        #I am duplicating the score calculation here, can rework this:
+        self.probabilities = np.float128(self.evaluateKDE())
+
         #Should remove this step and use the ln values given by the kde:
-        self.logLikelihoods = self.calculateLog2OfArray()
-        self.entropies = -self.probabilities*self.logLikelihoods
+        #self.log2Likelihoods = np.float128(self.calculateLog2OfArray())
+
+        self.entropies = np.float128(-self.probabilities*self.likelihoods)
         self.meanEntropy = np.mean(self.entropies)
         self.sdEntropy = np.std(self.entropies)
-        self.peaks = self.evaluatePeaks()
+        self.peaks = self.evaluatePeaks(sensitivity, width)
+        self.regionalEntropy = self.findRegionsOfHighEntropy(width = 2)
+        #To implement:
+        #outlier score and tau (cutoff value)
+        self.outliers, self.tau = self.findOutliers(alpha = 0.92)
 
+        #Outliers of regional/smoothed entropy:
+        self.entropyOutliers, self.entropyTau = self.findSmoothedEntropyOutliers(alpha = 0.94)
+        
+        #naive peakfinder
+        #combined methods...
+        ## Hillclimbing method from entropy peaks
 
     def getPeaks(self) -> list:
         return self.peaks
     def setPeaks(self, peaks:list) -> None:
         self.peaks = peaks
+    
 
     def estimateKernel(self) -> object:
         """
@@ -54,7 +79,7 @@ class spectrum:
         grid = GridSearchCV(KernelDensity(),
     {'bandwidth': h_vals, 'kernel': kernels},
     scoring=my_scores)
-
+        grid.fit(np.array(self.data[1]).reshape(-1, 1))
         #save best model
         kde = grid.best_estimator_
         return kde
@@ -63,13 +88,13 @@ class spectrum:
         """
         Evaluates the scaled probability density at each point in the spectrum
         """
-        return np.exp(self.kde.score_samples(self.data))
+        return np.float128(np.exp(self.kde.score_samples(np.array(self.data[1]).reshape(-1, 1))))
 
     def calculateLog2OfArray(self) -> np.array:
         """
         Calculates the log2 of the array of probability densities
         """
-        return np.log2(self.probabilities)
+        return np.float128(np.log2(self.probabilities))
        
         
 
@@ -86,7 +111,7 @@ class spectrum:
         logLikelihoods = self.logLikelihoods[start, stop]
         entropy = 0
         for probability in probabilities:
-            entropy += probability * np.log2(probability)
+            entropy += np.float128(probability) * np.float128(np.log2(probability))
 
         return -entropy
 
@@ -98,8 +123,35 @@ class spectrum:
         """
         return -self.entropies[index]
 
+    def findOutliers(self, alpha:float = 0.90) -> np.array:
+        """
+        This function calculates the alpha-th quantile of the KDE log Likilihoods. It uses this to filter out the normal data, and returns the indices of the outliers.py
 
-    
+        alpha: what quantile to calculate
+
+        Returns: np array of indices of outliers
+        """
+        #caluclate the alpha-quantile of the spectrum:
+        tau = mquantiles(self.likelihoods, 1. - alpha)
+        #this finds indexes where likelihood is less than tau, which then needs to be flattend because of the output format:
+        outliers= np.argwhere(self.likelihoods < tau).flatten()
+        print("Outliers found at locations: ", outliers)
+        return outliers, tau
+
+    def findSmoothedEntropyOutliers(self, alpha = 0.95):
+        """
+        This function calculates the alpha-th quantile of the smoothed entropies. It uses this to filter out the normal data, and returns the indices of the outliers.py
+
+        alpha: what quantile to calculate
+
+        Returns: np array of indices of outliers
+        """
+        #caluclate the alpha-quantile of the spectrum:
+        tau = mquantiles(self.regionalEntropy, 1. - alpha)
+        #this finds indexes where likelihood is less than tau, which then needs to be flattend because of the output format:
+        outliers= np.argwhere(self.regionalEntropy < tau).flatten()
+        print("Outliers found at locations: ", outliers)
+        return outliers, tau
 
     ######
     #Now, we have an object with a list of entropy differentials, and we just need to run the algorithm across that list. This should reduce the complexity from O(Length*WindowWidth) to O(Length+Length)
@@ -116,44 +168,152 @@ class spectrum:
         """
 
         #filter peaks out that are less than a certain number of SDs from the mean:
-        peaks = [index for index, entropy in enumerate(self.entropies) if (entropy - self.meanEntropy) > (sensitivity * self.sdEntropy)]
+        peaks = [index for index, entropy in enumerate(self.entropies) if -(entropy - self.meanEntropy) > (sensitivity * self.sdEntropy)]
 
         #Retain only one peak out of any set of peaks within k distance of each other:
         #Need to think about this and make it better:
+        #print(f"peaks found: {peaks}")
         filteredPeaks = []
         startingPoint = 0
         while startingPoint < len(peaks):
+            
             endingPoint = 1
-            currentGroup = {peaks[startingPoint]: self.data[peaks[startingPoint]]}
+
+            currentGroup = {peaks[startingPoint]: self.data[1][peaks[startingPoint]]}
+
             #find the group within k distance:
-            while np.abs(peaks[startingPoint] - peaks[startingPoint+endingPoint]) <= width:
-                currentGroup[peaks[startingPoint+endingPoint]] = self.data[peaks[startingPoint+endingPoint]]
+            while ((startingPoint+endingPoint) < len(peaks)) and (np.abs(peaks[startingPoint] - peaks[startingPoint+endingPoint]) <= width):
+                currentGroup[peaks[startingPoint+endingPoint]] = self.data[1][peaks[startingPoint+endingPoint]]
 
                 endingPoint += 1
             #find the bigest member of the group and add it to the list:
             filteredPeaks.append(max(currentGroup))
+
             
             #increment up the window:
             startingPoint += endingPoint
-        
+    
+
+        print(f"\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\nFound the following {len(filteredPeaks)} peaks: \n")
+        [print(i, " - ", self.data[1][i]) for i in filteredPeaks]
         return filteredPeaks
 
+    def findRegionsOfHighEntropy(self, width:float) -> list:
+        """
+        width: how wide of a band of wavelengths should be considered. this many right and left neigbours will be considered
+        sensitivity: How far from mean should a wavelength be in order to be counted.
+        """
+        #the starting locations of the sliding window:
+        center = width + 1
+        left = 0
+        right = 2 * width + 1
+
+        #initialize
+        regions = [self.meanEntropy for i in range(len(self.data))]
+        while right <= len(self.entropies):
+            region = self.entropies[left:right]
+            
+            regions[center] = np.mean(region)
+    
+            center += 1
+            left += 1
+            right += 1
+        return regions
+
+    #An algorithm that starts with outlier data and finds peaks using hillclimbing:
+    #could also use HDBSCAN to find clusters, and use cluster centroids as starting points
+    def climb(self, points, visibility=2):
+        """
+        """
+        spectrum = self.data[1]
+        peaks = []
+        ascending, index = True, 0
+        while index < len(points):
+            if ascending and index + 1 == len(points):#ascending and at and of data, ->peak, break out of loop
+                peaks.append(points[index])
+                break
+            elif ascending and spectrum[points[index]] > spectrum[points[index+1]]:#at a peak
+                peaks.append(points[index])
+                ascending = False
+            elif not ascending and spectrum[points[index]] <= spectrum[points[index+1]]:
+                ascending = True
+            index += 1
+        return peaks
 
         
 
+    #could also use HDBSCAN to find clusters, and use cluster centroids as starting points
+    def clusterOutliers(self, data):
+        """
+        clusters a one dimensional data set
+        """
+        model = OPTICS(min_samples=4).fit(data.reshape(-1, 1))
+        return model
 
+    def show(self, showPeaks = True, showEntropy = True):
+        plt.subplots(2,1)
+        plt.subplot(2,1,1)
+        plt.plot(self.data[1])
+        plt.title("Spectrum")
+        if showPeaks:
+            peakValues = [self.data[1][i] for i in self.peaks]
+            #print(peakValues)
+            plt.scatter(self.peaks, peakValues, c='red')
+        if showEntropy:
+
+            plt.subplot(2,1,2)
+            plt.plot(self.logLikelihoods)
+            [plt.axvline(self.peaks[i], c='red', alpha=0.5) for i in range(len(self.peaks))]
+        plt.show()
 
 
     
 """
 To Test:
-[] Instantiate a class
+[x] Instantiate a class
 [] do all features work
-    [] get Peaks
-    [] estimate Kernel
-    [] evaluate KDE
-    [] calculate log2 of array
+    [x] get Peaks
+    [x] estimate Kernel
+    [x] evaluate KDE
+    [x] calculate log2 of array
     []
 [] does the algorithm find peaks?
 [] can it handle large inputs
 """
+if __name__ == '__main__':
+    data, peaks = generate(n=2000, noise = 2)
+
+    #print(data[1])
+    spectrum1 = Spectrum(None, data=data, sensitivity=2)
+    #spectrum1.show(showPeaks=True)
+    #[print(spectrum1.entropies[peak]) for peak in peaks]
+    truePeaks = 0
+    falsePeaks = 0
+    for peak in spectrum1.peaks:
+        if peak in peaks:
+            truePeaks += 1
+        else: falsePeaks += 1
+    print(f'Correctly identified {truePeaks/len(peaks)}% of peaks')
+    print(f'Falsely identified {falsePeaks/len(spectrum1.peaks)}')
+    plt.subplots(3,1)
+    plt.subplot(311)
+    plt.plot(spectrum1.data[1])
+    #plt.scatter(x = spectrum1.outliers, y = spectrum1.data[1][spectrum1.outliers], c='red', alpha = 0.3)
+    model = spectrum1.clusterOutliers(spectrum1.entropyOutliers)
+    clusters = model.labels_
+    plt.scatter(x=spectrum1.entropyOutliers, y = spectrum1.data[1][spectrum1.entropyOutliers], c =clusters, alpha = 0.5, cmap='Set1')
+    plt.title("Spectrum showing outliers")
+    plt.subplot(312)
+    plt.plot(spectrum1.regionalEntropy)
+    plt.axhline(spectrum1.entropyTau, c = "red", alpha=0.6)
+    plt.title("Smoothed Entropies")
+    plt.subplot(313)
+    plt.plot(spectrum1.likelihoods)
+    plt.axhline(spectrum1.tau, c = "red", alpha=0.6)
+    plt.title("KDE Likelihoods and tau")
+    plt.tight_layout()
+    plt.show()
+
+    
+
+   
--- generateData.py
+++ generateData.py
@@ -1,21 +1,22 @@
 import numpy as np, matplotlib.pyplot as plt, pandas as pd
 
 
-def generate( n:int=1000, nPeaks:int = 10, noise:float = 1, peakwidthsVariability:float = 1) -> None:
+def generate( n:int=1000, nPeaks:int = 10, noise:float = 1, peakwidthsVariability:float = 1, peakHeightVariability:float = 20) -> None:
     """
     
     """
     peakCenters = np.random.randint(low=20, high=n-20,size=nPeaks)
-    minimumPeakWidth = 5-peakwidthsVariability
-    maximumPeakWidth = 5+peakwidthsVariability
+    minimumPeakWidth = 10-peakwidthsVariability
+    maximumPeakWidth = 10+peakwidthsVariability
     peakwidths = np.random.randint(low=minimumPeakWidth, high=maximumPeakWidth, size=nPeaks)
+    peakHeights = [np.random.rand() * peakHeightVariability + 90 for i in range(nPeaks)]
 
     #an initial string of random numbers centered on 0:
     data = np.random.normal(0,noise,n)
 
     #Make the peaks and add them to the data:
-    for center, width in zip(peakCenters, peakwidths):
-        peak = makePeak(width=width)
+    for center, width, height in zip(peakCenters, peakwidths, peakHeights):
+        peak = makePeak(width=width, height=height)
         for index1, index2 in enumerate(range(center-width, center+width+1)):
             data[index2] += peak[index1]
             
